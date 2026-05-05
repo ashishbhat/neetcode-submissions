d71a82dfd22348cd304de8321027948b99a8785b
Ashish Bhat <ashishbhat@users.noreply.github.com>
2026-05-05 17:04:14 +0530

    Add: anagram-groups - submission-9

---
+++ Data Structures & Algorithms/anagram-groups/submission-9.py
@@ -0,0 +1,20 @@
+class Solution:
+    def myHash(self,s: str) -> str:
+        freq = [0]*26
+        h = ""
+        for ch in s:
+            freq[ord(ch) - 97] += 1
+        print(freq)
+        return "".join([str(x)+"@" for x in freq])            
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+
+        for s in strs:
+            h = self.myHash(s)
+            anagrams[h].append(s)
+
+        return list(anagrams.values())
+
+
+        
