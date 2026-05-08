8c73e1f28d4b5b3c6a022ca7109b2041aacac4c6
Ashish Bhat <ashishbhat@users.noreply.github.com>
2026-05-08 16:06:24 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        java.util.Deque<Character> stack= new java.util.ArrayDeque<>();
+        int n = s.length();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')','(');
+        map.put(']','[');
+        map.put('}','{');
+
+        for(int i = 0; i < n; ++i){
+            char ch = s.charAt(i);
+            if(map.containsKey(ch)){
+                if(stack.peekLast() == map.get(ch)){
+                    stack.pollLast();
+                }else{
+                    return false;
+            }
+        }else{
+            stack.offerLast(ch);
+        }
+    }
+    if(stack.isEmpty()) return true;
+    return false;
+}
+}
