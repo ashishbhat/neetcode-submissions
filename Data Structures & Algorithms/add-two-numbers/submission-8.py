f355fa8b28edfa05a8635c0558de08309ee5f26e
Ashish Bhat <ashishbhat@users.noreply.github.com>
2026-05-16 18:37:57 +0530

    Add: add-two-numbers - submission-8

---
+++ Data Structures & Algorithms/add-two-numbers/submission-8.py
@@ -0,0 +1,68 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def getSize(self, head: Optional[ListNode]) -> int:
+        n = 0
+        while head:
+            n += 1
+            head = head.next
+        return n
+
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        n1 = self.getSize(l1)
+        n2 = self.getSize(l2)
+        l1_head = l1
+        l2_head = l2
+        
+        summ = carry = 0
+        while l1 != None and l2 != None:
+            summ = l1.val + l2.val + carry
+            if summ >= 10:
+                summ = summ % 10
+                carry = 1
+            else:
+                carry = 0
+            if n1 - n2 >= 0:
+                l1.val = summ
+            else:
+                l2.val = summ
+            
+            if l1.next == l2.next == None:
+                if carry:
+                    if n1 - n2 >= 0:
+                        l1.next = ListNode(1)
+                        return l1_head
+                    else:
+                        l1.next = ListNode(1)
+                        return l1_head
+                else:
+                    return l1_head if n1 - n2 >= 0 else l2_head
+
+            l1 = l1.next
+            l2 = l2.next
+            
+        temp = l1 if l2 is None else l2
+        while temp:
+            temp.val += carry
+            if temp.val >= 10:
+                temp.val %= 10
+                carry = 1
+            else:
+                carry = 0
+            if temp.next == None:
+                if carry:
+                    temp.next = ListNode(1)
+                break;
+            temp = temp.next
+
+
+        return l1_head if n1 - n2 >= 0 else l2_head
+
+
+
+
