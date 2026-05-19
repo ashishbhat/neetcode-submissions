21ddbf301c65c1df806a532e3a00ba733d28b26b
Ashish Bhat <ashishbhat@users.noreply.github.com>
2026-05-19 15:27:25 +0530

    Add: balanced-binary-tree - submission-5

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-5.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def __depth(self, root: Optional[TreeNode]) -> int:
+        if root == None:
+            return 0
+
+        l = self.__depth(root.left)
+        r = self.__depth(root.right)
+        if l == -1 or r == -1:
+            return -1
+        if abs(l - r) > 1:
+            return -1
+        #print(f'root={root.val} l_height={l},r_height={r}')
+        # print(f'root={root.val} height={max(l,r)}')
+
+        return 1 + max(l,r)
+
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if self.__depth(root) == -1:
+            return False
+        else:
+            return True
+
