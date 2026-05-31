84aea0f17b13634c338c6166d71a451f2b2283ba
Ashish Bhat <ashishbhat@users.noreply.github.com>
2026-05-31 09:51:17 +0530

    Add: task-scheduling - submission-12

---
+++ Data Structures & Algorithms/task-scheduling/submission-12.py
@@ -0,0 +1,28 @@
+import heapq
+from collections import deque,Counter
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        freq = Counter(tasks)
+        #create a max heap of task based on count
+        heap = [(-count, task, 0) for task, count in freq.items()]
+        heapq.heapify(heap)
+
+        cooldown = deque()
+        cycle = 0
+
+        while cooldown or heap:
+            cycle += 1
+
+            if cooldown and cooldown[0][2] <= cycle:
+                task = cooldown.popleft()
+                heapq.heappush(heap, task)
+                cycle = task[2]
+
+            if heap:
+                count, name, _ =  heapq.heappop(heap)
+                count += 1
+
+                if count != 0:
+                    cooldown.append((count, name, cycle + n + 1))
+
+        return cycle
