690dd2edfbac3e3ac5b048a6ea8c3385a5b039a6
Ashish Bhat <ashishbhat@users.noreply.github.com>
2026-06-10 16:34:25 +0530

    Add: count-number-of-islands - submission-5

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-5.py
@@ -0,0 +1,36 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        R = len(grid)
+        C = len(grid[0])
+        visited = [[False] * C for _ in range(R)]
+        num_islands = 0
+
+        #1. add each valid(not 0 and not visited) element to queue
+        #2. perform graph traversal from that point.
+        #3. return and increment the num_islands
+
+        def traverse(i: int, j: int) -> None:
+            queue = deque()
+            queue.append((i,j))
+            while queue:
+                r,c = queue.popleft()
+                if r == R or r < 0 or c == C or c < 0:
+                    continue
+                if visited[r][c] or grid[r][c] == "0":
+                    continue
+                visited[r][c] = True 
+                queue.append((r,c+1))
+                queue.append((r,c-1))
+                queue.append((r - 1,c))
+                queue.append((r + 1,c))
+            return 
+
+
+
+        for i in range(R):
+            for j in range(C):
+                if grid[i][j] == "1" and not visited[i][j]:
+                    traverse(i,j)
+                    num_islands += 1
+
+        return num_islands
