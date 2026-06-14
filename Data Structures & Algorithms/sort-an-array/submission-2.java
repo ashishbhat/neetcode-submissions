22f2b9adc1d58b1d85405a6560f7cbdb354226cf
Ashish Bhat <ashishbhat@users.noreply.github.com>
2026-06-15 00:00:33 +0530

    Add: sort-an-array - submission-2

---
+++ Data Structures & Algorithms/sort-an-array/submission-2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public void quickSort(int[] arr, int l, int r){
+        if( r - l <= 1){
+            return;
+        }
+        int i = l+1;
+        int j = l + 1;
+        int pivot = arr[l];
+        while(j < r){
+            if(arr[j] <= pivot){
+                swap(arr, i, j);
+                ++i;
+                ++j;
+            }else{
+                ++j;
+            }
+        }
+        swap(arr, i-1, l);
+        quickSort(arr, l, i-1);
+        quickSort(arr, i, r);
+    }
+    private void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public int[] sortArray(int[] nums) {
+        quickSort(nums, 0, nums.length);
+        return nums;
+    }
+}
