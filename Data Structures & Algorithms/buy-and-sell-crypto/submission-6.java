884d19d64ed35e1aae6a6de942d7deea215e265c
Ashish Bhat <ashishbhat@users.noreply.github.com>
2026-06-18 12:29:56 +0530

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int lowestPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i = 0; i < prices.length; ++i){
+            lowestPrice = Math.min(lowestPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - lowestPrice);
+        }
+        return maxProfit;
+    }
+}
