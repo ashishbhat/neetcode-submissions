e71f626eb70302fd25544fcdf2415301d7c1be3d
Ashish Bhat <ashishbhat@users.noreply.github.com>
2026-07-04 18:20:01 +0530

    Add: rotting-fruit - submission-16

---
+++ Data Structures & Algorithms/rotting-fruit/submission-16.py
@@ -0,0 +1,31 @@
+from collections import deque
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        queue = deque()
+        fresh = 0
+        time_taken = 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 2:
+                    queue.append((i, j))
+                if grid[i][j] == 1:
+                    fresh += 1
+
+        while queue:
+            n = len(queue)
+            for _ in range(n):
+                i, j = queue.popleft()
+                neighbours = [(i, j+1), (i, j-1), (i-1, j), (i+1,j)]
+                for i, j in neighbours:
+                    if  (0 <= i < len(grid) 
+                        and 0 <= j < len(grid[0]) 
+                        and grid[i][j] == 1
+                    ):
+                        grid[i][j] = 2
+                        queue.append((i,j))
+                        fresh -= 1
+            if queue:
+                time_taken += 1
+
+        return time_taken if fresh == 0 else -1 
