0eb98e1e332277f1e5ca7e5d0692f7ac4c59c0a0
Ashish Bhat <ashishbhat@users.noreply.github.com>
2026-07-08 16:43:33 +0530

    Add: network-delay-time - submission-2

---
+++ Data Structures & Algorithms/network-delay-time/submission-2.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+import heapq
+
+@dataclass
+class Edge:
+    to: int = -1
+    weight: int = -1
+
+@dataclass
+class Node:
+    time: int
+    n: int
+
+    def __lt__(self, other):
+        if isinstance(other,Node):
+            return self.time < other.time
+        return False
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        visited = set()
+        time = {i:float('inf') for i in range(1,n+1)}
+        time[k] = 0
+        g: dict[int, List[Edge]] = {i:[] for i in range(1,n+1)}
+
+        for u, v, t in times:
+            g[u].append(Edge(v,t))
+        print(g)
+
+        heap = []
+        heapq.heappush(heap, Node(0, k))
+
+        while heap:
+            fromm = heapq.heappop(heap)
+            if fromm.n in visited:
+                continue
+            visited.add(fromm.n)
+
+            for edge in g[fromm.n]:
+                if fromm.time + edge.weight < time[edge.to]:
+                    time[edge.to] = fromm.time + edge.weight
+                    heapq.heappush(heap, Node(time[edge.to], edge.to))
+        
+        print(time)
+        res = max(time.values())
+        return -1 if res == float('inf') else res
+
+
+
