d4767235fe38ee20205fa4435ec3f21f227c2123
Ashish Bhat <ashishbhat@users.noreply.github.com>
2026-07-08 14:58:38 +0530

    Add: redundant-connection - submission-10

---
+++ Data Structures & Algorithms/redundant-connection/submission-10.py
@@ -0,0 +1,43 @@
+class DSU:
+    def __init__(self, n: int) -> None:
+        self.root = [ i for i in range(n+1)]
+        self.sz = [1 for i in range(n+1)]
+        self.components = n
+
+    def find(self, a: int):
+        if self.root[a] != a:
+            self.root[a] = self.find(self.root[a])
+        return self.root[a]
+
+    def isConnected(self, a: int, b: int):
+        return self.find(a) == self.find(b)
+    
+    def union(self, a: int, b: int) -> bool:
+        ra, rb = self.find(a), self.find(b)
+
+        if ra == rb:
+            return False
+        
+        if self.sz[ra] > self.sz[rb]:
+            rb, ra = ra, rb
+        self.root[ra] = rb
+        self.sz[rb] += self.sz[ra]
+        self.components -= 1
+
+        return True
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        dsu = DSU(len(edges))
+
+        for u, v in edges:
+            if not dsu.union(u, v):
+                return [u, v]
+        return []
+
+        
+
+
+
+        
