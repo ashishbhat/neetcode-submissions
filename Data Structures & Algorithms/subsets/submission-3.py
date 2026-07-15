1a25b861539be4a8ac73cebff9435ecb2961f927
Ashish Bhat <ashishbhat@users.noreply.github.com>
2026-07-15 17:23:04 +0530

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    @staticmethod
+    def helper(nums, subsets, curr, i, end):
+        if i == end:
+            subsets.append(curr.copy())
+            return
+
+        Solution.helper(nums, subsets, curr.copy(), i+1, end)
+        curr.append(nums[i])
+        Solution.helper(nums, subsets, curr.copy(), i+1, end)
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        subsets: List[List[int]] = []
+        current: List[int] = []
+        i = 0
+        end = len(nums)
+        Solution.helper(nums, subsets, current, i, end)
+        return subsets
