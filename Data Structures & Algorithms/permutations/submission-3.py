db2524804c820adece881e4ef2dfbbf5156666d0
Ashish Bhat <ashishbhat@users.noreply.github.com>
2026-07-15 23:20:30 +0530

    Add: permutations - submission-3

---
+++ Data Structures & Algorithms/permutations/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        current = set(nums)
+        x = []
+        def helper():
+            if not current:
+                result.append(x.copy())
+                return
+
+            for i in list(current):
+                current.discard(i)
+                x.append(i)
+                helper()
+                x.pop()
+                current.add(i)
+        helper()
+        return result
