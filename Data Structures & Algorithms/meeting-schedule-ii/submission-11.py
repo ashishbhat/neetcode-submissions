774d72c7fafb7dca0c70bc4dcd173ed78f399b66
Ashish Bhat <ashishbhat@users.noreply.github.com>
2026-08-02 19:39:09 +0530

    Add: meeting-schedule-ii - submission-11

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-11.py
@@ -0,0 +1,24 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+        intervals.sort(key = lambda x : x.start)
+        heap = []
+        max_rooms = 0
+
+        for interval in intervals:
+            while heap and heap[0] <= interval.start:
+                heapq.heappop(heap)
+            heapq.heappush(heap, interval.end)
+            max_rooms = max(max_rooms, len(heap))
+        return max_rooms
+
+                
